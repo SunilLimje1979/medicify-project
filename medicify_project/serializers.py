951b4ce5b013ec438e4707fc1eac35b805aa9393
unknown <SunilLimje1979>
2024-02-12 18:07:26 +0530

    modual intial commit

---
+++ medicify_project/serializers.py
@@ -0,0 +1,171 @@
+from rest_framework import serializers
+from .models import Tbldoctorappointments, TblmedicineInstructions, Tbldatacodemaster, TblpatientFindingsandsymtoms, TblpatientLabinvestigations, TblpatientMedications,Tblpatientbiometrics,Tblconsultations, Tblprescriptions
+from .models import TbldoctorMedicines
+from .models import Tbldoctorlocations
+from .models import Tbldoctors  # Import the correct model
+from .models import Tbldoctorlocationavailability
+from .models import TblpateintCharges, TblpatientPayments
+from .models import TblpatientPayments
+from .models import *
+
+
+class TbldoctorappointmentsSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Tbldoctorappointments
+        fields = '__all__'
+
+class TblmedicineInstructionsSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = TblmedicineInstructions
+        fields = '__all__'
+
+
+class DataCodeMasterSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Tbldatacodemaster
+        fields = '__all__'
+
+class PatientBiometricsSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Tblpatientbiometrics
+        fields = '__all__'
+
+class ConsultationSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Tblconsultations
+        fields = '__all__'
+
+class PrescriptionsSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Tblprescriptions
+        fields = '__all__'
+
+
+class FindingsandsymtomsSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = TblpatientFindingsandsymtoms
+        fields = '__all__'
+
+class MedicationsSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = TblpatientMedications
+        fields = '__all__'
+
+class LabinvestigationsSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = TblpatientLabinvestigations
+        fields = '__all__'
+
+
+######################### Doctor Medicines ############################
+class TbldoctorMedicinesSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = TbldoctorMedicines
+        fields = '__all__'
+        
+######################### Doctor Locations ############################        
+class DoctorLocationSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Tbldoctorlocations
+        fields = '__all__'
+        
+######################### Doctor ############################         
+class DoctorSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Tbldoctors  # Use the correct model
+        fields = '__all__'
+        
+######################### Doctor location Availability ############################    
+class DoctorLocationAvailabilitySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Tbldoctorlocationavailability
+        fields = '__all__'
+        
+######################### Patient Charges ############################   
+class PatientChargesSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = TblpateintCharges
+        fields = '__all__'
+        
+######################### Patient Payments ############################           
+class TblpatientPaymentsSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = TblpatientPayments
+        fields = '__all__'
+
+######################### Doctor Appointments ############################           
+class TbldoctorappointmentsSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Tbldoctorappointments
+        fields = '__all__'
+
+######################### Appointment ############################   
+class AppointmentSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Tbldoctorappointments
+        fields = ['doctor_id', 'appointment_datetime', 'appointment_name', 'appointment_mobileno', 'appointment_gender']
+
+######################### Patient Vitals ############################   
+class TblPatientVitalsSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Tblpatientvitals
+        fields = '__all__'
+
+######################### Patients ############################   
+class TblpatientsSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Tblpatients
+        fields = '__all__'
+ 
+ ######################### Patient Charges ############################      
+
+
+
+######################### Patient Payments ############################   
+class TblpatientPaymentsSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = TblpatientPayments
+        fields = ['doctor_id', 'patient_id', 'patient_status', 'payment_mode', 'payment_amount', 'payment_transaction_no','isdeleted']
+
+######################### Patient ############################   
+class TblPatientsSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Tblpatients
+        fields = [
+            'patient_mobileno', 'patient_firstname', 'patient_lastname',
+            'patient_fateherhusbandname', 'patient_gender', 'patient_dateofbirth',
+            'patient_maritalstatus', 'patient_aadharnumber', 'patient_universalhealthid',
+            'patient_bloodgroup', 'patient_level', 'patient_emergencycontact',
+            'patient_address', 'patient_cityid', 'patient_stateid', 'patient_countryid',
+            'isdeleted','istestpatient',
+        ]
+
+######################### Patient Findingsandsymtoms ############################   
+class TblpatientFindingsandsymtomsSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = TblpatientFindingsandsymtoms
+        fields = [
+            'patient_id',
+            'doctor_id',
+            'patient_status',
+            'findgings_datetime',
+            'consultation_id',
+            'findings',
+            'symtoms',
+            'isdeleted',
+        ]
+
+######################### Patient Complaints ############################   
+class TblpatientComplaintsSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = TblpatientComplaints
+        fields = [
+            'doctor_id',
+            'patient_id',
+            'complaint_datetime',
+            'complaint_details',
+            'appointment_id',
+            'consultation_id',
+            'isdeleted',
+        ]
+
---
+++ medicify_project/views.py
@@ -0,0 +1,6 @@
+from django.http import JsonResponse
+from django.shortcuts import render, HttpResponse
+
+# Create your views here.
+def hello(request):
+    return HttpResponse("hello")
